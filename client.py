df8584d025de293c4aed0451251aad9922e44436
Marshallov <marshall.2410adv@gmail.com>
2026-04-16 13:23:41 +0300

    new_task

--- client.py
+++ client.py
@@ -1,14 +1,21 @@
 import socket
-import pickle
+import random
+from utils import xor_encrypt, xor_decrypt
 
 HOST = '127.0.0.1'
-PORT = 8080
+PORT = 5000
 
-sock = socket.socket()
-sock.connect((HOST, PORT))
+p = 23
+g = 5
 
-p, g, a = 7, 5, 3
-A = g ** a % p
-sock.send(pickle.dumps((p, g, A)))
-
-sock.close()
+client_secret = random.randint(1, 100)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    A = pow(g, client_secret, p)
+    s.send(str(A).encode())
+    B = int(s.recv(1024).decode())
+    K = pow(B, client_secret, p)
+    message = 'Hello server&quot'   
+    s.send(xor_encrypt(message, K))
+    response = xor_decrypt(s.recv(1024), K)
+    print(f'[CLIENT] Response: {response}')
--- server.py
+++ server.py
@@ -1,15 +1,27 @@
 import socket
-import pickle
+import random
+from utils import xor_decrypt, xor_encrypt
 
 HOST = '127.0.0.1'
-PORT = 8080
+PORT = 5001
 
-sock = socket.socket()
-sock.bind((HOST, PORT))
-sock.listen(1)
-conn, addr = sock.accept()
+p = 23
+g = 5
 
-msg = conn.recv(1024)
-print(pickle.loads(msg))
+server_secret = random.randint(1, 100)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    print('[SERVER] Waiting...')
+conn, addr = s.accept()
 
-conn.close()
+with conn:
+    A = int(conn.recv(1024).decode())
+    B = pow(g, server_secret, p)
+    conn.send(str(B).encode())
+    K = pow(A, server_secret, p)
+    encrypted = conn.recv(1024)
+    message = xor_decrypt(encrypted, K)
+    print('[SERVER] Received: {message}')
+    response = 'OK'
+    conn.send(xor_encrypt(response, K))
